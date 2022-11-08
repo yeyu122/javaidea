26cf64ae50de73a299e26607ecf84e0a40a21c6d
yeyu122 <2771262114@qq.com>
2022-11-08 11:49:52 +0800

    提交

--- src/day01/domecj_08.java
+++ src/day01/domecj_08.java
@@ -2,12 +2,12 @@ package day01;
 
 import java.util.Scanner;
 
-public class dome_08 {
+public class domecj_08 {
     public static void main(String[] args) {
        Scanner a= new Scanner(System.in);
        System.out.println("请输入您的成绩:");
        int cj = a.nextInt();
-       if (cj>=90&&cj<100){
+       if (cj>=90&&cj<=100){
            System.out.println("优秀");
        }else if (cj>=80&&cj<90){
            System.out.println("良好");
